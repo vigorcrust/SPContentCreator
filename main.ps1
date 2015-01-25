8a1e710fa983e572ab4f249de7d8c57b98c31c03
Daniel Martens <ddmartens@gmail.com>
2015-01-26 00:02:08 +0100

    Remodel the main script to execute the scripts.

--- main.ps1
+++ main.ps1
@@ -6,12 +6,12 @@ param(
 if ($script:apperror){exit}
 
 # For development purposes
-Remove-Module XmlRecursive -ErrorAction SilentlyContinue
 cls
 
 # Import nesessary Modules
 import system.utils.convert
 import system.xml
+import sharepoint.custom
 
 # Variable declaration
 $incrementalAttributes = "url" # Creates a "i_url" in $ComplexObject.Persistent
@@ -36,8 +36,9 @@ Select-Xml -Xml $configAsXml -XPath "//*/@type" | Select-Object -ExpandProperty 
 
     if ($attributes.count -ne 0){
         Write-Host "Call Method: $($ComplexObject.Key)" -ForegroundColor Green
+        $object = New-Object PSObject
         foreach ($att in $attributes){
-            "$($att.LocalName) - $($att.'#text')"
+            $object | Add-Member Noteproperty $att.LocalName $att.'#text'
         }
         for($i=0; $i -le $ComplexObject.Level; $i++){
             if ($ComplexObject.Persistent.i_url.count -gt 0){
@@ -46,8 +47,9 @@ Select-Xml -Xml $configAsXml -XPath "//*/@type" | Select-Object -ExpandProperty 
                 }
             }
         }
+        $object | Add-Member Noteproperty WorkingUrl $url
         
-        Write-Host "$url" -ForegroundColor Magenta
+        Invoke-Function -functionToCall $complexObject.Key -objectToPass $object
     }
 }
 
