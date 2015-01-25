2aea26838a7eff47df171afcda7d98a1406282df
Daniel Martens <ddmartens@gmail.com>
2015-01-25 12:23:15 +0100

    Cleanup example code and added some documentation.

--- main.ps1
+++ main.ps1
@@ -1,6 +1,7 @@
 . "$(split-path -parent $MyInvocation.MyCommand.Definition)\import\helper.ps1"
 if ($script:apperror){exit}
 
+# Import nesessary Modules
 import system.utils.convert
 
-Convert-JsonToXml
+# Main App starts here
