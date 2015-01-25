673274ec318b6da5725be9f441841e770e4b7c2b
Daniel Martens <ddmartens@gmail.com>
2015-01-25 14:33:02 +0100

    Testing the invoke-xmlrecursive function with the sample strcture and a temporary test function.

--- main.ps1
+++ main.ps1
@@ -7,8 +7,12 @@ if ($script:apperror){exit}
 
 # Import nesessary Modules
 import system.utils.convert
+import system.xml
 
 # Main App starts here
 
+function test{"called"}
+
 # Import Json file and convert to xml
-$configAsXml = Convert-JsonToXml -json $(Get-Content -Path $ConfigFile -Raw)
+$configAsXml = Convert-JsonToXml -Json $(Get-Content -Path $ConfigFile -Raw)
+Invoke-XmlRecursive -XmlDocOrElement $configAsXml -FunctionToCall test
