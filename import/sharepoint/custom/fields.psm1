e44c4e294c8d04a757c713ed3bfc035ca08522c0
Daniel Martens <ddmartens@gmail.com>
2015-01-26 00:36:17 +0100

    Beta versions of contenttype and field, which may be the most complex, but best to try out.

--- import/sharepoint/custom/fields.psm1
+++ import/sharepoint/custom/fields.psm1
@@ -1,3 +1,25 @@
 function fields{
     param($complexObject)
+
+    $spWeb = Get-SPWeb $complexObject.WorkingUrl
+    $ct = $spWeb.ContentTypes[$complexObject.Detailed.Parent.name]
+
+    if(!$complexObject.wildcardfields -and $complexObject.wildcardfields -ne ""){
+        $wildCardFields = $spWeb.Fields | where {$_.Title -like $complexObject.wildcardfields}
+        foreach ($field in $wildCardFields) {
+            $link = New-Object Microsoft.SharePoint.SPFieldLink $field
+            $createdCT.FieldLinks.Add($link)    
+        }
+    }
+    if(!$complexObject.matchfield -and $complexObject.matchfield -ne ""){
+        $wildCardFields = $spWeb.Fields | where {$_.Title -eq $complexObject.matchfield}
+        foreach ($field in $matchfield) {
+            $link = New-Object Microsoft.SharePoint.SPFieldLink $field
+            $createdCT.FieldLinks.Add($link)    
+        }
+    }
+
+    $ct.Update()
+    
+
 }
