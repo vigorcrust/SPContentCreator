767b949c3fda8e89eccbc72573b6ce889144a601
Daniel Martens <ddmartens@gmail.com>
2015-01-25 17:40:29 +0100

    More extensive parsing.

--- import/system/xml/XmlRecursive.psm1
+++ import/system/xml/XmlRecursive.psm1
@@ -1,7 +1,9 @@
 function Invoke-XmlRecursive{
     param(
         $XmlDocOrElement,
-        [String]$FunctionToCall
+        [String]$FunctionToCall,
+        $IncrementalAttributes,
+        $PersistentData = $(New-Object PSObject)
     )
 	foreach ($node in $XmlDocOrElement.ChildNodes){
         if($node.NodeType -ne "Text"){
@@ -10,12 +12,26 @@ function Invoke-XmlRecursive{
                 $nodeValue = $node.'#text'
             }
             $object = New-Object PSObject                                       
-            $object | add-member Noteproperty Key $nodeName
-            $object | add-member Noteproperty Value $nodeValue
-            $object | add-member Noteproperty Node $node
+            $object | Add-Member NoteProperty Key $nodeName
+            $object | Add-Member NoteProperty Value $nodeValue
+            $object | Add-Member NoteProperty Node $node
+
+            if ($IncrementalAttributes -contains $nodeName){
+                Write-Host "Found $nodeName" -ForegroundColor Red
+                $PersistentData | Add-Member Noteproperty "i_$($nodeName)" $($PersistentData."i_$nodeName" + $nodeValue) -Force
+            }
+            
+            $object | Add-Member NoteProperty Persistent $PersistentData
+
+            $parentAttributes = $XmlDocOrElement.SelectNodes("./*[not(*)]")
+            $parentAttributeObject = New-Object PSObject
+            foreach ($attribute in $parentAttributes){
+                $parentAttributeObject | Add-Member Noteproperty $attribute.LocalName $attribute.'#text'
+            }
+            $object | Add-Member Noteproperty Parent $parentAttributeObject
 
             Invoke-Function -FunctionToCall $FunctionToCall -ObjectToPass $object
         }
-		Invoke-XmlRecursive -xmlDoc $node -functionToCall $FunctionToCall
+		Invoke-XmlRecursive -xmlDoc $node -functionToCall $FunctionToCall -IncrementalAttributes $IncrementalAttributes -PersistentData $PersistentData
 	}
 }
--- main.ps1
+++ main.ps1
@@ -5,21 +5,32 @@ param(
 . "$(split-path -parent $MyInvocation.MyCommand.Definition)\import\helper.ps1"
 if ($script:apperror){exit}
 
+Remove-Module XmlRecursive
+
 # Import nesessary Modules
 import system.utils.convert
 import system.xml
 
 # Main App starts here
-
-function processNode{
+ function processNode{
     param(
-        $ComplexObject   #$complexObject.Key $complexObject.Value $complexObject.Node
+        $ComplexObject   #$complexObject.Key $complexObject.Value $complexObject.Node $complexObject.Parent
     )
-    "$($complexObject.Key) - $($complexObject.Value)"
+
+    $attributes = $ComplexObject.Node.SelectNodes("./*[not(*)]")
+
+    if ($attributes.count -ne 0){
+        Write-Host "Call Method: $($ComplexObject.Key)" -ForegroundColor Green
+        Write-Host "Parent Url: $($ComplexObject.Parent.url)" -ForegroundColor Yellow
+        foreach ($att in $attributes){
+            "$($att.LocalName) - $($att.'#text')"
+        }
+        Write-Host "$($ComplexObject.Persistent.i_url)" -ForegroundColor Magenta
+    }
 }
 
-# $incrementalAttributes = "url"
+$incrementalAttributes = "url" # Creates a "i_url" in $ComplexObject.IncrementalAttributes
 
 # Import Json file and convert to xml
 $configAsXml = Convert-JsonToXml -Json $(Get-Content -Path $ConfigFile -Raw)
-Invoke-XmlRecursive -XmlDocOrElement $configAsXml.FirstChild -FunctionToCall processNode
+Invoke-XmlRecursive -XmlDocOrElement $configAsXml.FirstChild -FunctionToCall processNode -IncrementalAttributes $incrementalAttributes
